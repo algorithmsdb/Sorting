436e50fdc32c087a61f546a889205ede171944ed
algorithmsdb <algorithmsdb@users.noreply.github.com>
2014-06-14 21:08:15 +0530

    Create Sort Strings Case Sensitive.cpp

---
+++ Sort Strings Case Sensitive.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct icompare_char 
+{
+  bool operator()(char c1, char c2)
+  {
+      int x=abs(c1-c2);
+      if(x==32){ return c2 < c1;}
+    return c1 < c2;
+  }
+};
+
+// return true if s1 comes before s2
+struct compare
+{
+  bool operator()(std::string const& s1, std::string const& s2) {
+
+    return std::lexicographical_compare(s1.begin(), s1.end(),
+                                        s2.begin(), s2.end(),
+                                        icompare_char());
+  }
+};
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<string> m;
+    string i;
+    while(n--)
+    {
+        cin>>i;
+        m.push_back(i);
+    }
+    sort(m.begin(),m.end(),compare());
+
+    vector<string>::iterator it;
+    for(it=m.begin();it!=m.end();it++)
+    {
+        cout<<*it<<"\n";
+    }
+}
+
